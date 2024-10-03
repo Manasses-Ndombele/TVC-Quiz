ab7d5d297a66fef1d06c9fbb6a50aa6605b1c410
Manasses-Ndombele <manassesndombelefreelancer@gmail.com>
2024-10-03 09:30:46 +0200

    Comprimi as imagens, adicionei o link da landing page e resolvi alguns bugs na página de resultados e no gráfico

--- tvc_quiz_app/static/js/results.js
+++ tvc_quiz_app/static/js/results.js
@@ -1,6 +1,8 @@
 let nextStepBtns = document.querySelectorAll('button.next-step-btn')
 let optionsBtns = document.querySelectorAll('button.option.option-btn')
 let mainNextStepBtns = []
+let titleResult = document.querySelector('h2#title-result')
+let descriptionResult = document.querySelectorAll('p.description-result')
 
 nextStepBtns.forEach(btn => { mainNextStepBtns.push(btn) })
 optionsBtns.forEach(btn => { mainNextStepBtns.push(btn) })
@@ -55,6 +57,15 @@ xhr.onreadystatechange = () => {
                   const yesAnswersCount = yesAnswersMatches ? yesAnswersMatches.length : 0
                   const notAnswersMatches = answers.match(/false/g)
                   const notAnswersCount = notAnswersMatches ? notAnswersMatches.length : 0
+                  if (yesAnswersCount > 3) {
+                        titleResult.innerHTML = '<strong>VOCÊ</strong> ESTÁ <strong>VICIADO NA PORNOGRAFIA</strong>'
+                        descriptionResult[0].textContent = 'Você respondeu SIM a 3 ou mais questões.'
+                        descriptionResult[1].textContent = 'Isso significa, segundo os critérios da OMS você se enquadra na definição médica de dependência (vício) em pornografia e pode ser considerado viciado.'
+                  } else if (notAnswersCount > 3) {
+                        titleResult.innerHTML = '<strong>VOCÊ NÃO</strong> ESTÁ <strong>VICIADO NA PORNOGRAFIA</strong>'
+                        descriptionResult[0].textContent = 'Você respondeu NÃO para a maioria das questões.'
+                        descriptionResult[1].textContent = 'São necessárias pelo menos 03 questões SIM para o diagnóstico de vício. Você não se enquadra na definição médica de dependência (vício) em pornografia.'
+                  }
                   setChart(yesAnswersCount, notAnswersCount)
             }
       }
